162866a0e2f10276fbac0d1a5063693568ae88ad
Mugisha-Elie <iradukunda2k24@gmail.com>
2025-11-17 08:27:26 +0200

    react: conditional rendering

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,8 +2,16 @@
 import './index.css';
 
 const TodoList = ({todos}) => {
-    const renderedTodo = todos.map(todo => {
-        return <div key={todo.id}>{todo.title}</div>
+    // const renderedTodo = todos.map(todo => {
+    //     return <div key={todo.id}>{todo.title}</div>
+    // })
+
+    const renderedTodo = todos.map(({id, title, completed}) => {
+        return <div key={id}>
+            {completed && '✔'}
+            {completed || '✖'} 
+            {title}
+        </div> 
     })
 
     return <section>{renderedTodo}</section>
